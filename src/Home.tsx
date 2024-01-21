c6f99da599e80ee65a06191e5a2cf5a864785663
ArthurSalt <arthursalt1990@gmail.com>
2024-01-21 16:16:09 +0300

    feat: router start

---
+++ src/Home.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { TextField } from "./components/ui/textfield";
+import { LoginForm } from "./components/auth/login-form/login-form";
+
+export function Home() {
+  const [value, setValue] = useState('')
+  console.log("render")
+
+  return <div>
+    <p>Hello</p>
+    <Button as='button' variant="link">Button</Button>
+    <Button as='a' href="https://www.google.ru/" target="blank" variant="secondary">Button Link</Button>
+    <TextField value={value} onChange={e => setValue(e.target.value)} placeholder={""} variant={"primary"} />
+    <LoginForm/>
+
+  </div>
+}
--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,10 +1,9 @@
-import { useForm, useController } from 'react-hook-form'
-import {DevTool} from '@hookform/devtools'
+import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 import z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { Button } from '../../ui/button'
-import { TextField } from '@/components/ui/textfield'
 
 import styles from './login-form.module.scss'
 import { ControlledCheckbox } from '@/components/ui/controlled/controlled-checkbox/controlled-checkbox'
@@ -18,27 +17,33 @@ const loginSchema = z.object({
   rememberMe: z.boolean().default(false),
 })
 
+// useForm shoul have default values for all the controlled inputs.
+// Default values could be made via Zod as well
+// use useController to make an input controllable and refactor it to separate folder.
 export const LoginForm = () => {
-  const { handleSubmit, control, formState: { errors }, } = useForm<FormValues>({ resolver: zodResolver(loginSchema) })
+  const { handleSubmit, control, formState: { errors }, } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onChange', // form validation mode
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false
+    }
+  })
 
   const onSubmit = (data: FormValues) => {
     console.log(data)
   }
 
-
-  const { field: email } = useController({
-    name: 'email',
-    control,
-    defaultValue: '',
-  })
-
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.loginForm}>
-      <DevTool control={control}/>
-      <ControlledTextField control={control} name={'email'} errorMassage={errors.email?.message} variant={'primary'} label={'Email'} placeholder={'enter email'}/>
-      <ControlledTextField control={control} name={'password'} errorMassage={errors.password?.message} variant={'secondary'} label={'Password'} placeholder={'enter password'}/>
-      <ControlledCheckbox label={'remember me'} control={control} name={'rememberMe'}/>
+      {/* RHF Devtool for Storybook*/}
+      <DevTool control={control} />
+      {/* RHF DevTool for Storybook*/}
+
+      <ControlledTextField control={control} name={'email'} errorMassage={errors.email?.message} variant={'primary'} label={'Email'} placeholder={'enter email'} />
+      <ControlledTextField control={control} name={'password'} errorMassage={errors.password?.message} variant={'secondary'} label={'Password'} placeholder={'enter password'} />
+      <ControlledCheckbox label={'remember me'} control={control} name={'rememberMe'} />
       <Button type="submit">Submit</Button>
     </form>
   )
